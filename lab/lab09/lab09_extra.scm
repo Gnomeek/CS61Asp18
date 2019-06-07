9fbf2d3a87b20c187da321140e33337401fea759
Gnomeek <gnomeek@outlook.com>
2019-06-07 13:52:27 +0800

    lab09 all done

--- lab/lab09/lab09_extra.scm
+++ lab/lab09/lab09_extra.scm
@@ -45,15 +45,23 @@
 
 ; Q9
 (define (no-repeats s)
-  (filter (lambda (x) (not (= x (car s)))) (cdr s))
+  (cond ((null? s) nil)
+  		(else (cons (car s) (no-repeats (filter (lambda (x) (not (= x (car s)))) (cdr s)))))
+  )
 )
 
 ; Q10
 (define (substitute s old new)
-  'YOUR-CODE-HERE
+  (cond ((null? s) nil)
+  		((pair? (car s)) (cons (substitute (car s) old new) (substitute (cdr s) old new)))
+  		((equal? (car s) old) (cons new (substitute (cdr s) old new)))
+  		(else (cons (car s) (substitute (cdr s) old new)))
+  )
 )
 
 ; Q11
 (define (sub-all s olds news)
-  'YOUR-CODE-HERE
+  (cond ((null? olds) s)
+  		(else (sub-all (substitute s (car olds) (car news)) (cdr olds) (cdr news)))
+  )  
 )
