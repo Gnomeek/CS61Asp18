8e2e5596a498566c023ac4a50f06f775c38a174a
Gnomeek <gnomeek@outlook.com>
2019-06-18 23:40:56 +0800

    hw10

--- homework/hw10/hw10.scm
+++ homework/hw10/hw10.scm
@@ -1,14 +1,16 @@
 (define (accumulate combiner start n term)
   (if (= n 0)
       start
-      'YOUR-CODE-HERE
+      (combiner (term n) (accumulate combiner start (- n 1) term))
   )
 )
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  (if (= n 0)
+  	  start
+  	  (accumulate-tail combiner (combiner start (term n)) (- n 1) term))
 )
 
 (define-macro (list-of expr for var in seq if filter-fn)
-  'YOUR-CODE-HERE
+  `(map (lambda (,var) ,expr) (filter (lambda (,var) ,filter-fn) ,seq))
 )
